2a59e189a00b3aed61be582ee65d4e91a26f905e
npratik0 <neupanepratik00@gmail.com>
2025-12-17 10:46:35 +0545

    counter

--- lib/screens/provider_state_provider_screen.dart
+++ lib/screens/provider_state_provider_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 
 // 1. Provider Ref: Provider -> Provider
 // 2. Widget Ref: Provider -> Widget
@@ -9,11 +10,23 @@ final appbarProvider = Provider<String>((ref) {
   return "Provider and StateProvider";
 });
 
-class ProviderStateProviderScreen extends StatelessWidget {
+final definationProvider = Provider<String>((ref) {
+  return "Provider is a popular, officially recommended package for state management that simplifies how data is shared and updated across the widget tree";
+});
+
+final definationStateProvider = Provider<String>((ref) {
+  return "The StateProvider is a feature of the Riverpod state management package for Flutter, designed to manage and expose simple, mutable data types such as integers, strings, booleans, or enums.";
+});
+
+final counterProvider = StateProvider<int>((ref) {
+  return 0;
+});
+
+class ProviderStateProviderScreen extends ConsumerWidget {
   const ProviderStateProviderScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
@@ -40,34 +53,34 @@ class ProviderStateProviderScreen extends StatelessWidget {
                       icon: const Icon(Icons.arrow_back, color: Colors.white),
                     ),
                     const SizedBox(width: 8),
-                    Consumer(
-                      builder: (context, ref, child) {
-                        final appBarTitle = ref.read(appbarProvider);
-                        return Text(
-                          appBarTitle,
-                          style: Theme.of(context).textTheme.titleLarge
-                              ?.copyWith(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                              ),
-                        );
-                      },
-                    ),
+                    Text(ref.read(appbarProvider)),
+                    const SizedBox(width: 8),
                   ],
                 ),
+                // SizedBox(child: Column(
+                //   children: [
+                //     Text(ref.read(definationProvider)),
+                //     const SizedBox(height: 8),
+                //     Text(ref.read(definationStateProvider)),
+                //   ],
+                // ))
               ),
               Expanded(
                 child: Center(
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
+                      Text(ref.read(definationProvider)),
+                      const SizedBox(height: 8),
+                      Text(ref.read(definationStateProvider)),
+                      const SizedBox(height: 8),
                       const Text(
                         'Counter',
                         style: TextStyle(color: Colors.white70, fontSize: 18),
                       ),
                       const SizedBox(height: 16),
                       Text(
-                        '0',
+                        '${ref.watch(counterProvider)}',
                         style: Theme.of(
                           context,
                         ).textTheme.displayLarge?.copyWith(color: Colors.white),
@@ -85,21 +98,27 @@ class ProviderStateProviderScreen extends StatelessWidget {
         children: [
           FloatingActionButton(
             heroTag: 'decrement',
-            onPressed: () {},
+            onPressed: () {
+              ref.read(counterProvider.notifier).state--;
+            },
             backgroundColor: Colors.red,
             child: const Icon(Icons.remove),
           ),
           const SizedBox(width: 16),
           FloatingActionButton(
             heroTag: 'reset',
-            onPressed: () {},
+            onPressed: () {
+              ref.read(counterProvider.notifier).state = 0;
+            },
             backgroundColor: Colors.grey,
             child: const Icon(Icons.refresh),
           ),
           const SizedBox(width: 16),
           FloatingActionButton(
             heroTag: 'increment',
-            onPressed: () {},
+            onPressed: () {
+              ref.read(counterProvider.notifier).state++;
+            },
             backgroundColor: Colors.green,
             child: const Icon(Icons.add),
           ),
